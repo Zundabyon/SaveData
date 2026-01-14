50e55a294d8148eaa682522a70aac0777dcf2a49
metappi <paperdju2@gmail.com>
2026-01-14 17:15:32 +0900

    scaffold実行にてCLUD作成

---
+++ db/migrate/20260114073020_create_users.rb
@@ -0,0 +1,13 @@
+class CreateUsers < ActiveRecord::Migration[7.2]
+  def change
+    create_table :users do |t|
+      t.string :email
+      t.string :name
+      t.date :birthday
+      t.boolean :gender
+      t.integer :crypted_password
+
+      t.timestamps
+    end
+  end
+end
