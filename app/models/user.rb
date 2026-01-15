1e754d1e9f6f609ecc3b489fcbc4e333627429cc
metappi <paperdju2@gmail.com>
2026-01-15 13:22:08 +0900

    gem annotate対応

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,15 @@
+# == Schema Information
+#
+# Table name: users
+#
+#  id               :bigint           not null, primary key
+#  birthday         :date
+#  crypted_password :integer
+#  email            :string
+#  gender           :boolean
+#  name             :string
+#  created_at       :datetime         not null
+#  updated_at       :datetime         not null
+#
 class User < ApplicationRecord
 end
--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -1,3 +1,16 @@
+# == Schema Information
+#
+# Table name: users
+#
+#  id               :bigint           not null, primary key
+#  birthday         :date
+#  crypted_password :integer
+#  email            :string
+#  gender           :boolean
+#  name             :string
+#  created_at       :datetime         not null
+#  updated_at       :datetime         not null
+#
 require "test_helper"
 
 class UserTest < ActiveSupport::TestCase
