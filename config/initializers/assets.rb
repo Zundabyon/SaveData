96d88bf2f5cc5a50b6df7bdb4ff19d16aead906b
metappi <paperdju2@gmail.com>
2026-02-08 15:17:31 +0900

    tailwind.cssをプリコンパイル対象に追加

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -1,6 +1,7 @@
 # config/initializers/assets.rb
 
 Rails.application.config.assets.version = "1.0"
-
-# SassCコンプレッサーを無効化
 Rails.application.config.assets.css_compressor = nil
+
+# tailwind.css を追加
+Rails.application.config.assets.precompile += %w( tailwind.css application.css )
