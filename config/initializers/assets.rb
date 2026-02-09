5fab5c0811da64e39732205269309ba86c807440
metappi <paperdju2@gmail.com>
2026-02-09 14:59:45 +0900

    Ensure assets/builds in asset paths and fix manifest/layout for tailwind

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -5,3 +5,6 @@ Rails.application.config.assets.css_compressor = nil
 
 # tailwind.css を追加
 Rails.application.config.assets.precompile += %w( tailwind.css application.css )
+
+# include built assets (esbuild / Tailwind CLI output)
+Rails.application.config.assets.paths << Rails.root.join("app", "assets", "builds")
