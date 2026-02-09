690b150c1fbe7dcbab23238500dfcf6901c6e371
metappi <paperdju2@gmail.com>
2026-02-09 17:08:19 +0900

    Enable runtime asset compilation to avoid build-time SECRET_KEY_BASE requirement

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -8,7 +8,7 @@ Rails.application.configure do
   # Full error reports are disabled and caching is turned on.
   config.consider_all_requests_local = false
   config.action_controller.perform_caching = true
-  config.assets.compile = false
+  config.assets.compile = true
 
   config.active_storage.service = :cloudinary
 
