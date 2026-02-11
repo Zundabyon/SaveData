1b3b691090c56cae3544508c705d5fd5db1e904e
metappi <paperdju2@gmail.com>
2026-02-11 15:12:34 +0900

    last_impression

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -7,7 +7,7 @@ class ApplicationController < ActionController::Base
   protected
 
   def after_sign_in_path_for(resource)
-    authenticated_root_path  # または dashboard_path
+    authenticated_root_path
   end
 
   def configure_permitted_parameters
