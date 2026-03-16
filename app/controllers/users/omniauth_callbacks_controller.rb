588c8f899e28d776cf385823ff9a782f994aec0d
metappi <paperdju2@gmail.com>
2026-03-16 14:57:29 +0900

    googleログインできない不具合解消

--- app/controllers/users/omniauth_callbacks_controller.rb
+++ app/controllers/users/omniauth_callbacks_controller.rb
@@ -1,13 +1,20 @@
 class Users::OmniauthCallbacksController < Devise::OmniauthCallbacksController
   def google_oauth2
-    @user = User.from_omniauth(request.env["omniauth.auth"])
+    begin
+      Rails.logger.info "OmniAuth auth data: #{request.env["omniauth.auth"].inspect}"
+      @user = User.from_omniauth(request.env["omniauth.auth"])
 
-    if @user.persisted?
-      sign_in_and_redirect @user, event: :authentication
-      set_flash_message(:notice, :success, kind: "Google") if is_navigational_format?
-    else
-      session["devise.google_data"] = request.env["omniauth.auth"].except("extra")
-      redirect_to new_user_registration_url, alert: @user.errors.full_messages.join(", ")
+      if @user.persisted?
+        sign_in_and_redirect @user, event: :authentication
+        set_flash_message(:notice, :success, kind: "Google") if is_navigational_format?
+      else
+        session["devise.google_data"] = request.env["omniauth.auth"].except("extra")
+        redirect_to new_user_registration_url, alert: @user.errors.full_messages.join(", ")
+      end
+    rescue => e
+      Rails.logger.error "Google OAuth error: #{e.message}"
+      Rails.logger.error e.backtrace.join("\n")
+      redirect_to root_path, alert: "Googleログインでエラーが発生しました。#{e.message}"
     end
   end
 
