e766c9d0d588193db182e71008b2540035ebb61f
metappi <paperdju2@gmail.com>
2026-03-21 23:15:07 +0900

    ユーザーページ実装

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,19 @@
 module ApplicationHelper
+
+def job_image_filename(job)
+  job_images = {
+    "せんし" => "warrior.png", "まほうつかい" => "mage.png",
+    "とうぞく" => "thief.png", "そうりょ" => "priest.png",
+    "ゆうしゃ" => "fighter.png", "しにがみ" => "devil.png",
+    "Figmaしょくにん" => "figma_master.png", "ロブスター" => "lobster.png",
+    "その他" => "other.png",
+    "warrior" => "warrior.png", "mage" => "mage.png",
+    "thief" => "thief.png", "priest" => "priest.png",
+    "fighter" => "fighter.png", "devil" => "devil.png",
+    "figma_master" => "figma_master.png", "lobster" => "lobster.png",
+    "other" => "other.png"
+  }
+  job_images[job] || "other.png"
+end
+
 end
