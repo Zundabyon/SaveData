13961aff1506a7a82f973f18911ec7f72e946273
metappi <paperdju2@gmail.com>
2026-03-23 10:41:08 +0900

    X

--- app/javascript/share_card.js
+++ app/javascript/share_card.js
@@ -3,11 +3,14 @@ import html2canvas from "html2canvas"
 window.shareCard = function(gameId) {
   const card = document.getElementById(`game-card-${gameId}`)
 
-html2canvas(card, {
-  useCORS: true,
-  allowTaint: true,
-  scale: 1
-}).then(canvas => {
+  // 先にウィンドウを開く（ポップアップブロック対策）
+  const tweetWindow = window.open('', '_blank')
+
+  html2canvas(card, {
+    useCORS: true,
+    allowTaint: true,
+    scale: 1
+  }).then(canvas => {
     const imageData = canvas.toDataURL('image/png')
 
     fetch('/share_images', {
@@ -20,14 +23,13 @@ html2canvas(card, {
     })
     .then(res => res.json())
     .then(data => {
-      // TinyURLで短縮
       fetch(`https://tinyurl.com/api-create.php?url=${encodeURIComponent(data.url)}`)
         .then(res => res.text())
         .then(shortUrl => {
           const tweetUrl = `https://twitter.com/intent/tweet?text=SaveDataでゲームの思い出を記録したよ！%0A${encodeURIComponent(shortUrl)}`
-          window.open(tweetUrl, '_blank')
+          tweetWindow.location.href = tweetUrl
         })
     })
-    .catch(e => console.log('エラー:', e)) // ← 追加
+    .catch(e => console.log('エラー:', e))
   })
 }
