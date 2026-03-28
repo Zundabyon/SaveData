a165585652b57cbb59a66b16beee8e949a4a983a
metappi <paperdju2@gmail.com>
2026-03-29 01:39:19 +0900

    8テスト実行

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -2,20 +2,70 @@ require 'rails_helper'
 
 RSpec.describe User, type: :model do
   describe 'バリデーション' do
+
     context 'emailがあるとき' do
       it '有効であること' do
-        # emailありのユーザーを作る
         user = build(:user)
-        # 有効か確認
         expect(user).to be_valid
       end
     end
 
     context 'emailがないとき' do
       it '無効であること' do
-        # emailなしのユーザーを作る
         user = build(:user, email: nil)
-        # 無効か確認
+        expect(user).not_to be_valid
+      end
+    end
+
+    context 'nameがあるとき' do
+      it '有効であること' do
+        user = build(:user, name: "John Doe")
+        expect(user).to be_valid
+      end
+    end
+
+    context 'nameがないとき' do
+      it '無効であること' do
+        user = build(:user, name: nil)
+        expect(user).not_to be_valid
+      end
+    end
+
+    context 'passwordがあるとき' do
+      it '有効であること' do
+        user = build(:user, password: "password")
+        expect(user).to be_valid
+      end
+    end
+    context 'passwordがないとき' do
+      it '無効であること' do
+        user = build(:user, password: nil)
+        expect(user).not_to be_valid
+      end
+    end
+
+      context 'genderがあるとき' do
+      it '有効であること' do
+        user = build(:user, gender: "male")
+        expect(user).to be_valid
+      end
+    end
+    context 'genderがないとき' do
+      it '無効であること' do
+        user = build(:user, gender: nil)
+        expect(user).not_to be_valid
+      end
+    end
+    
+    context 'jobがあるとき' do
+      it '有効であること' do
+        user = build(:user, job: "engineer")
+        expect(user).to be_valid
+      end
+    end
+    context 'jobがないとき' do
+      it '無効であること' do
+        user = build(:user, job: nil)
         expect(user).not_to be_valid
       end
     end
