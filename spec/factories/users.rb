ae094fdfec1819481ce896cbdbf0a84c7cb8b7ff
metappi <paperdju2@gmail.com>
2026-03-29 16:00:17 +0900

    Googleアカウント登録周りのテスト

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -6,5 +6,17 @@ FactoryBot.define do
     birthday    { Faker::Date.birthday }
     gender      { :male }
     job         { Faker::Job.title }
+
+  # 追加する時はtraitを定義して、必要な属性を上書きする
+  # google_oauth2でのユーザーを作成するためのtraitです
+  # userだけでなく、Googleログインを通して作ったユーザーも必要なため
+  # providerとuidはomniauthでユーザーを作成するために必要な属性です
+  trait :google_oauth2 do
+    provider { "google_oauth2" }
+    uid { Faker::Number.number(digits: 10).to_s }
+    birthday { nil }
+    gender { nil }
+    job { nil }
   end
+ end
 end
